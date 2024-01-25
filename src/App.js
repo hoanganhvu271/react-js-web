ec1cc43bf41047bc17ac6bcbd97542076cf24d71
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-01-25 16:54:09 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Vu"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Helo</h1>
+        <h1> Helo {name}</h1>
       </header>
     </div>
   );
