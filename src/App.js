25225d75623a45d2d58d13247f6e523f70774d13
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-01-26 17:22:02 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav.js';
+import { useState, useEffect } from 'react';
+import Todo from './views/Todo.js';
+
 
 function App() {
-  let name = "Vu"
+  // let name = "Vu"
+  const [name, setName] = useState('Vu')
+  const [address, setAddress] = useState('')
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'todo1', type: 't1' },
+    { id: '2', title: 'todo2', type: 't2' }
+  ])
+
+  // chay khi giao dien re - rendering
+  useEffect(() => {
+    console.log('useEffect')
+  }, [address]);
+
+  const onClickHandler = (event) => {
+    if (!address) {
+      alert('empty');
+      return;
+    }
+    let newTodo = { id: Math.floor(Math.random() * 1000 + 1), title: address, type: 't1' }
+    setTodos([...todos, newTodo])
+    setAddress('')
+
+  }
+  const onChangeHandler = (event) => {
+    setAddress(event.target.value)
+    // console.log(event.target.value)
+  }
+
+  const deleteDataTodo = (id) => {
+    let currentDataTodo = todos
+    currentDataTodo = currentDataTodo.filter(item => item.id !== id)
+    setTodos(currentDataTodo)
+  }
+
   return (
+
     <div className="App">
+
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Helo {name}</h1>
-      </header>
-    </div>
+
+        <Todo
+          todos={todos}
+          title='All todo list'
+          deleteDataTodo={deleteDataTodo}
+        />
+
+        <Todo
+          todos={todos.filter(item => item.type === 't2')}
+          title="t2's todo list"
+          deleteDataTodo={deleteDataTodo}
+        />
+
+        <input type='text' value={address} onChange={(event) => onChangeHandler(event)} />
+        <button type='button' onClick={(event) => onClickHandler(event)}>  Click !!!</button>
+      </header >
+    </div >
+
   );
 }
 
---
+++ src/views/Todo.js
@@ -0,0 +1,34 @@
+const Todo = (props) => {
+
+    //properties
+    const todos = props.todos
+    const handleDelete = (id) => {
+        props.deleteDataTodo(id)
+    }
+    return (
+
+        <div className='todo-container' >
+            <div className="title">
+                {props.title}
+            </div>
+            {
+                todos.map(todo => {
+                    return (
+                        <div className="todo-child" key={todo.id} >
+                            <li> {todo.title}
+                                &nbsp; <span onClick={() => handleDelete(todo.id)}>X</span>
+                            </li>
+
+
+                        </div>
+
+                    )
+
+                })
+            }
+            <hr></hr>
+        </div >
+    )
+}
+
+export default Todo
